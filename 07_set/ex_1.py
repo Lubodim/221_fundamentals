1fe8e8a4967115d31540738438c0b385d4a43803
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2023-11-10 10:51:13 +0200

    add new files

---
+++ 07_set/ex_1.py
@@ -0,0 +1,65 @@
+my_set = set(input() for _ in range(int(input())))
+while my_set:
+    print(my_set.pop())
+
+#
+# my_set = set()
+#
+# for _ in range(int(input())):
+#     current_name = input()
+#     my_set.add(current_name)
+#
+# print(my_set)
+#
+#
+# while my_list:
+#     print(my_list.pop())
+
+
+
+
+
+
+# my_list = []
+#
+# for _ in range(int(input())):
+#     current_name = input()
+#     if current_name not in my_list:
+#         my_list.append(current_name)
+# print(my_list)
+#
+#
+# while my_list:
+#     print(my_list.pop())
+
+
+# n = int(input("Enter the number"))
+# my_list = []
+#
+# for _ in range(n):
+#     current_name = input()
+#     if current_name not in my_list:
+#         my_list.append(current_name)
+# print(my_list)
+#
+#
+# while my_list:
+#     print(my_list.pop())
+
+
+
+# n = int(input("Enter the number"))
+# my_list = []
+# unic_list = []
+# for _ in range(n):
+#     current_name = input()
+#     my_list.append(current_name)
+# print(my_list)
+# for i in range(len(my_list)):
+#     current_n = my_list[i]
+#     if current_n not in unic_list:
+#         unic_list.append(current_n)
+# print(unic_list)
+#
+# while unic_list:
+#     print(unic_list.pop())
