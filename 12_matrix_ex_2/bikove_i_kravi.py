7d5aed8e678fa59829c6209f585b877dbf943be6
Lyubomir Dimitrov <lyubomir.dimitrov.8702@gmail.com>
2024-01-10 12:37:01 +0200

    change file

--- 12_matrix_ex_2/bikove_i_kravi.py
+++ 12_matrix_ex_2/bikove_i_kravi.py
@@ -1,27 +1,36 @@
-player_1_name = input()
-player_2_name = input()
+from random import sample
 
-player_1_num = "1234"
-player_2_num = "4321"
-player_1_count = 0
-player_2_count = 0
-gues_1 = input()
-gues_2 = input()
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+player_1_name = input("Enter a name of first player: ")
+player_2_name = input("Enter a name of second player: ")
+
+player_1_num = "".join(str(x) for x in sample(numbers, 4))
+player_2_num = "".join(str(x) for x in sample(numbers, 4))
+
+print(player_1_num)
+print(player_2_num)
 while True:
-    if gues_1 == player_2_num:
-        print(f"{player_1_name} is winner")
-        break
-    if gues_2 == player_1_num:
-        print(f"{player_2_name} is winner")
-        break
-    for ch, i in enumerate(player_1_num):
-        for c, j in enumerate(player_2_num):
+
+    gues = input(f"{player_1_name.upper()} must enter a gues number: ")
+
+    if gues == player_2_num:
+        print(f"Result for {player_1_name.upper()} gueses: ")
+        print(("Бик \n" * 4))
+        print(f"{player_1_name.upper()} is winner")
+        exit()
+    print(f"Result for {player_1_name.upper()} gueses: ")
+    for i, ch in enumerate(player_2_num):
+        for j, c in enumerate(gues):
             if c == ch and i == j:
                 print("Бик")
-                continue
             elif c == ch:
                 print("Крава")
 
-    gues_1 = input()
-    gues_2 = input()
+    print()
 
+    player_1_name, player_2_name = player_2_name, player_1_name
+    player_1_num, player_2_num = player_2_num, player_1_num
+
+# ml = ["a", "b", "c", "d", "e", "f"]
+# for j, c in enumerate(ml):
+#     print(f"index - {j} element {c}")
